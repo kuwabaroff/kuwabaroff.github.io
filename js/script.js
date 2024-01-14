fb5ec973b18b2594b3c7c50149db04365a939c4d
Arthur Osmolovsky <kuwabaroff@gmail.com>
2024-01-14 13:55:20 +0300

    Update 0.01

--- js/script.js
+++
@@ -1,8 +0,0 @@
-var wallet_text = document.getElementById('wallet-text');
-var add_button = document.getElementById('add-button');
-var wallet = 0;
-
-add_button.addEventListener('click', function() {
-    wallet++;
-    wallet_text.textContent = wallet;
-});
